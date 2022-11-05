a9d1259aaffc7ad9dee19f4934ebfa99e3dd0e83
unknown <akhishah5@gmail.com>
2022-11-05 16:01:12 +0530

    add api code

--- student-api.php
+++ student-api.php
@@ -1,2 +1,25 @@
 <?php 
-echo "Student Api Data";
+
+$servername = "localhost";
+$username = "u805731630_apis";
+$password = "Api@@2022";
+$dbname = "u805731630_apis";
+
+ $conn = new mysqli($servername, $username, $password, $dbname);
+//print_r($conn);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT `id`, `name`, `email`, `mobile` FROM new_api";
+$result = $conn->query($sql);
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+    
+}
